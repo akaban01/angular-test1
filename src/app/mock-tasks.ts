3b06c92f0e67a3c15f885916a9d49d6b633d0eb8
Faizan Rauf <frauf@cyracom.com>
2021-11-05 22:57:39 -0700

    initial app structure

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Dr apt",
+        day: "11/5/2021",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Dr apt2",
+        day: "11/6/2021",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Dr apt3",
+        day: "11/7/2021",
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Dr apt4",
+        day: "11/8/2021",
+        reminder: true
+    },
+    {
+        id: 5,
+        text: "Dr apt5",
+        day: "11/9/2021",
+        reminder: true
+    }
+]
